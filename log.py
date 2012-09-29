87a6f986540bea5cf917d5ab672fc72d9a19be8f
Balthazar Rouberol <rouberol.b@gmail.com>
2012-09-29 15:31:47 +0200

    MOAR COMMENTS! COMMENT ALL THE LINES! Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- log.py
+++ log.py
@@ -14,7 +14,7 @@ def file_console_log():
         * the console stdout stream, in which all events
           are displayed.
     """
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)  # new logger object
     logger.setLevel(logging.DEBUG)  # redirect all events to handlers
 
     # Formatter
@@ -24,13 +24,13 @@ def file_console_log():
     log_path = join(dirname(__file__), 'scripts', 'retrieve_tweets.log')
     file_handler = logging.FileHandler(log_path)
     file_handler.setLevel(logging.INFO)  # debug messages will not be logged
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)  # apply format
     logger.addHandler(file_handler)
 
-    # Stream handler: display all events
+    # Stream handler: display all events in the console
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)  # debug messages will de displayed
-    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)  # all messages will de displayed
+    stream_handler.setFormatter(formatter)  # apply format
     logger.addHandler(stream_handler)
 
     return logger
